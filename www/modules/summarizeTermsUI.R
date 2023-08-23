64b4b4c5df99702c59e924b1cedac5eff6440bac
Julius Fenn <camel.app@proton.me>
2023-08-23 10:20:29 +0200

    spelling

--- www/modules/summarizeTermsUI.R
+++ www/modules/summarizeTermsUI.R
@@ -12,7 +12,7 @@ actionButton(inputId =  ns("ST_approxMatch"), label = HTML('Approximate<br>match
 actionButton(inputId =  ns("ST_searchTerms"), label = "Searching terms",
              icon = icon(name = "angle-right", lib = "font-awesome"),
              class = "btn-sidebar"),
-actionButton(inputId = ns("ST_synonyms"), label = HTML("Search for<br>Synonyms"),
+actionButton(inputId = ns("ST_synonyms"), label = HTML("Search for<br>synonyms"),
              icon = icon(name = "angle-right", lib = "font-awesome"),
              class = "btn-sidebar"),
 actionButton(inputId = ns("ST_wordVec"), label = HTML("Apply word2vec<br>model"),
